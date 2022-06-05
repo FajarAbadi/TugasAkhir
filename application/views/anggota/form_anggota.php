b2454346e6785c64124bc5d35e29abff38cfa294
FajarAbadi <fajarfitra111@gmail.com>
2022-06-05 21:22:58 +0700

    update lagi

--- application/views/anggota/form_anggota.php
+++ application/views/anggota/form_anggota.php
@@ -14,7 +14,7 @@
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <label for="inputPassword3" class="col-sm-2 control-label">Nama Anggota</label>
 
                 <div class="col-sm-10">
                     <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
