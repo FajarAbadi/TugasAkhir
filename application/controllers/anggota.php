b7c2ec766d15d3b6b0f6e0706fe2fbf94715cee0
FajarAbadi <fajarfitra111@gmail.com>
2022-06-05 21:51:32 +0700

    penambahan baru di anggota.php

--- application/controllers/anggota.php
+++ application/controllers/anggota.php
@@ -1,7 +1,13 @@
 <?php
 
-class Anggota extends CI_Controller{
+class Anggota extends CI_Controller
+{
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_anggota');
+    }
     public function index()
     {
         $isi['content'] = 'anggota/v_anggota';
@@ -11,8 +17,9 @@ class Anggota extends CI_Controller{
 
     public function tambah_anggota()
     {
-        $isi['content'] = 'anggota/form_anggota';
-        $isi['judul']   = 'Form Tambah Anggota';
-        $this->load->view('v_dashboard',$isi);
+        $isi['content']     = 'anggota/form_anggota';
+        $isi['judul']       = 'Form Tambah Anggota';
+        $isi['id_anggota']  =
+            $this->load->view('v_dashboard', $isi);
     }
 }
