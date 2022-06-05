c40e3e15f3cbf65a8130ec2988dd1f0e5c147d1b
FajarAbadi <fajarfitra111@gmail.com>
2022-06-05 21:21:14 +0700

    update form_anggota.php

--- application/views/anggota/form_anggota.php
+++ application/views/anggota/form_anggota.php
@@ -10,7 +10,7 @@
                 <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 
                 <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    <input type="text" name="id_anggota" class="form-control" readonly>
                 </div>
             </div>
             <div class="form-group">
