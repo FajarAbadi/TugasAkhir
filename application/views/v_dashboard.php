1e6e4c4141c078d471b4a0f0ee098627c090a5b3
FajarAbadi <fajarfitra111@gmail.com>
2022-06-05 20:52:06 +0700

    coba

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -7,17 +7,17 @@
   <title>AdminLTE 2 | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="<?php base_url()?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/bower_components/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/bower_components/Ionicons/css/ionicons.min.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/dist/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/dist/css/skins/_all-skins.min.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/bower_components/morris.js/morris.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/bower_components/jvectormap/jquery-jvectormap.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-  <link rel="stylesheet" href="<?php base_url()?>assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/morris.js/morris.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -80,24 +80,24 @@
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-<script src="<?php base_url()?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="<?php base_url()?>assets/bower_components/raphael/raphael.min.js"></script>
-<script src="<?php base_url()?>assets/bower_components/morris.js/morris.min.js"></script>
-<script src="<?php base_url()?>assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<script src="<?php base_url()?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="<?php base_url()?>assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<script src="<?php base_url()?>assets/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<script src="<?php base_url()?>assets/bower_components/moment/min/moment.min.js"></script>
-<script src="<?php base_url()?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<script src="<?php base_url()?>assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<script src="<?php base_url()?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<script src="<?php base_url()?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="<?php base_url()?>assets/bower_components/fastclick/lib/fastclick.js"></script>
-<script src="<?php base_url()?>assets/dist/js/adminlte.min.js"></script>
-<script src="<?php base_url()?>assets/dist/js/pages/dashboard.js"></script>
-<script src="<?php base_url()?>assets/dist/js/demo.js"></script>
-<script src="<?php base_url()?>assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="<?php base_url()?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/raphael/raphael.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/morris.js/morris.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="<?php echo base_url();?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo base_url();?>assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/moment/min/moment.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo base_url();?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="<?php echo base_url();?>assets/dist/js/adminlte.min.js"></script>
+<script src="<?php echo base_url();?>assets/dist/js/pages/dashboard.js"></script>
+<script src="<?php echo base_url();?>assets/dist/js/demo.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url();?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <script>
   $(function () {
     $('#example1').DataTable()
